22a5d6694526b63334a326889b87eca3d53d8a88
Karl Stolley <karl.stolley@gmail.com>
2017-06-14 18:15:24 -0500

    Improve routes using `match` syntax

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,7 @@
 Rails.application.routes.draw do
-  get 'survey/:survey_token', to: 'survey#index', constraints: {
-    survey_token: /[0-9a-f]{10,}/ }
-
-  put 'survey/:survey_token', to: 'survey#update', as: :survey
+  match 'survey/:survey_token', to: 'survey#index', constraints: {
+    survey_token: /[0-9a-f]{10,}/ }, via: [:get]
+  match 'survey/:survey_token', to: 'survey#update', via: [:put, :patch], as: :survey
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
