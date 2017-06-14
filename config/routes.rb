780aae3e6a0185114df55b312903fa349f07c719
Karl Stolley <karl.stolley@gmail.com>
2017-06-14 15:02:15 -0500

    Add put route for survey response

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,7 @@ Rails.application.routes.draw do
   get 'survey/:survey_token', to: 'survey#index', constraints: {
     survey_token: /[0-9a-f]{10,}/ }
 
+  put 'survey/:survey_token', to: 'survey#update', as: :survey
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
