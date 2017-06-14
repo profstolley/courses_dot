cdc363a42ed6e09a8af3916b0630c5775a596f6c
Karl Stolley <karl.stolley@gmail.com>
2017-06-14 07:42:18 -0500

    Run Student model migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170614123553) do
+ActiveRecord::Schema.define(version: 20170614123942) do
 
   create_table "courses", force: :cascade do |t|
     t.string "designation"
@@ -20,6 +20,17 @@ ActiveRecord::Schema.define(version: 20170614123553) do
     t.string "site_repo"
     t.string "demo_repo"
     t.string "work_repo"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "students", force: :cascade do |t|
+    t.string "roster_first"
+    t.string "roster_last"
+    t.string "preferred_first"
+    t.string "university_email"
+    t.string "preferred_email"
+    t.string "github_username"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
