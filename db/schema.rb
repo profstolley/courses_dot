8f44e67398a06484353fc53d339be60243ab9f14
Karl Stolley <karl.stolley@gmail.com>
2017-06-14 07:49:29 -0500

    Run Enrollment model migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170614123942) do
+ActiveRecord::Schema.define(version: 20170614124426) do
 
   create_table "courses", force: :cascade do |t|
     t.string "designation"
@@ -22,6 +22,18 @@ ActiveRecord::Schema.define(version: 20170614123942) do
     t.string "work_repo"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "enrollments", force: :cascade do |t|
+    t.integer "course_id"
+    t.integer "student_id"
+    t.boolean "contract_acceptance"
+    t.string "term"
+    t.string "survey_token"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["course_id"], name: "index_enrollments_on_course_id"
+    t.index ["student_id"], name: "index_enrollments_on_student_id"
   end
 
   create_table "students", force: :cascade do |t|
