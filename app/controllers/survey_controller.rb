832978a0e8f55877b0b60f8758743018bb232727
Karl Stolley <karl.stolley@gmail.com>
2017-06-14 22:57:50 -0500

    Add remaining controller methods and setup method
    Also added a simple "Thanks" view that needs improvement (obviously).

--- app/controllers/survey_controller.rb
+++ app/controllers/survey_controller.rb
@@ -1,9 +1,21 @@
 class SurveyController < ApplicationController
+  before_action :set_survey, except: [:thanks]
+
   def index
-    @survey = Enrollment.find_by_survey_token(params[:survey_token])
+  end
+
+  def update
+  end
+
+  def thanks
   end
 
   private
+
+  def set_survey
+    @survey = Enrollment.find_by_survey_token(params[:survey_token])
+  end
+
   def survey_params
     params.require(:enrollment).permit(:survey_token, :contract_acceptance, student_attributes: :preferred_first)
   end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   match 'survey/:survey_token', to: 'survey#index', constraints: {
     survey_token: /[0-9a-f]{10,}/ }, via: [:get]
   match 'survey/:survey_token', to: 'survey#update', via: [:put, :patch], as: :survey
-
+  get 'survey/thanks', to: 'survey#thanks', as: :survey_thanks
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
