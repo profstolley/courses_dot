39fc6d8e19b4d14f37b747ef81723d76e8948b40
Karl Stolley <karl.stolley@gmail.com>
2017-06-14 22:52:43 -0500

    Correctly handle nested params
    Note the changes across the model (accepts_nested_attributes_for), and
    the setup with the private survey_params method on the controller.
    
    Also, the view now correctly references fields_for as a method on the
    parent form object (f) to ensure properly nested parameters.

--- app/controllers/survey_controller.rb
+++ app/controllers/survey_controller.rb
@@ -2,4 +2,9 @@ class SurveyController < ApplicationController
   def index
     @survey = Enrollment.find_by_survey_token(params[:survey_token])
   end
+
+  private
+  def survey_params
+    params.require(:enrollment).permit(:survey_token, :contract_acceptance, student_attributes: :preferred_first)
+  end
 end
--- app/models/enrollment.rb
+++ app/models/enrollment.rb
@@ -1,4 +1,5 @@
 class Enrollment < ApplicationRecord
   belongs_to :course
   belongs_to :student
+  accepts_nested_attributes_for :student
 end
